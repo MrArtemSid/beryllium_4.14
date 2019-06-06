a5207cc35cbd784d5fc5e2861218f3f812de737c
Angelo G. Del Regno <kholk11@gmail.com>
2019-06-06 12:13:40 +0200

    irqchip: arm-gic-v3: Implement GIC save and restore API for SDM845
    On the SDM845 SoC we require to save and restore SPI to
    keep functionality when resuming the GIC as it would lose
    the configuration when sent to sleep.
    
    This is a port of kernel 4.9 commit
    b179a8e1e9f8c085f64fb0b60f1244a882fad01e
    
    Provide api to save/restore GICD state for SPIs. SPI
    configuration is restored for GICD_ICFGR, GICD_ISENABLER,
    GICD_IPRIORITYR, GICD_IROUTER registers. Following is the
    sequence for restore:
    
    1. For SPIs, check whether any of GICD_ICFGR, GICD_ISENABLER,
       GICD_IPRIORITYR, GICD_IROUTER, current configuration is
       different from saved configuration.
    
    For all irqs, with mismatched configurations,
    
    2. Set GICD_ICENABLER and wait for its completion.
    
    3. Restore any changed GICD_ICFGR, GICD_IPRIORITYR, GICD_IROUTER
       configurations.
    
    4. Set GICD_ICACTIVER.
    
    5. Set pending for the interrupt.
    
    6. Enable interrupt and wait for its completion.
    
    [ab123321]
    Fix merge conflicts and apply 3d5764bcb6c9ffd1237a54bf2ebeff2c3426b568
    and acbe915710e71c7418da631123adfc29de2e64e1
    
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -599,6 +599,9 @@ static inline bool gic_enable_sre(void)
 
 void gic_show_pending_irqs(void);
 unsigned int get_gic_highpri_irq(void);
+void gic_v3_dist_save(void);
+void gic_v3_dist_restore(void);
+
 #endif
 
 #endif
