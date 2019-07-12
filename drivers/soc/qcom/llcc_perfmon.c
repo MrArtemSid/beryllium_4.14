ded7f5279d60fb98d23309d7fdf7b790fb1d7f9e
Angelo G. Del Regno <kholk11@gmail.com>
2019-07-12 12:37:47 +0200

    soc: qcom: llcc_perfmon: Do tell which error happened if clk get fails
    If clock get fails, it may be because of a probe deferral or
    because of whatever else: do tell!

--- drivers/soc/qcom/llcc_perfmon.c
+++ drivers/soc/qcom/llcc_perfmon.c
@@ -1279,8 +1279,9 @@ static int llcc_perfmon_probe(struct platform_device *pdev)
 
 	llcc_priv->clock = devm_clk_get(&pdev->dev, "qdss_clk");
 	if (IS_ERR(llcc_priv->clock)) {
-		pr_err("failed to get clock node\n");
-		return PTR_ERR(llcc_priv->clock);
+		result = PTR_ERR(llcc_priv->clock);
+		pr_err("failed to get clock node: %d\n", result);
+		return result;
 	}
 
 	llcc_priv->version = REV_0;
