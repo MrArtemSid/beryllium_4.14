dcf886f97a9ab07dc162963b5e2b509f3f6f8934
rupansh <rupanshsekar@hotmail.com>
2020-03-10 21:41:01 +0000

    thermal: fix compilation
    Signed-off-by: rupansh <rupanshsekar@hotmail.com>

--- drivers/thermal/thermal_core.h
+++ drivers/thermal/thermal_core.h
@@ -173,13 +173,10 @@ int of_thermal_aggregate_trip(struct thermal_zone_device *tz,
 			      enum thermal_trip_type type,
 			      int *low, int *high);
 void of_thermal_handle_trip(struct thermal_zone_device *tz);
-<<<<<<< HEAD
 void of_thermal_handle_trip_temp(struct thermal_zone_device *tz,
 					int trip_temp);
-=======
 int of_parse_thermal_message(void);
 void free_thermal_message(void);
->>>>>>> 4685495544c8... drivers: thermal: Import Xiaomi changes
 #else
 static inline int of_parse_thermal_zones(void) { return 0; }
 static inline void of_thermal_destroy_zones(void) { }
