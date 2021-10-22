6071bfad4ea30e28c389b4ebf7c9cca220e04139
MrArtemSid <artem.sid.key@gmail.com>
2021-10-23 00:12:03 +0400

    Revert "input: nt36xxx: Disable firmware update"
    This reverts commit bfb71ba25c49a825d75782cd6ee899fab9d5145e.

--- drivers/input/touchscreen/nt36xxx/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.h
@@ -85,7 +85,7 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 #if WAKEUP_GESTURE
 extern const uint16_t gesture_key_array[];
 #endif
-#define BOOT_UPDATE_FIRMWARE 0
+#define BOOT_UPDATE_FIRMWARE 1
 #define BOOT_UPDATE_FIRMWARE_NAME "novatek_nt36672_e10.fw"
 
 //---ESD Protect.---
