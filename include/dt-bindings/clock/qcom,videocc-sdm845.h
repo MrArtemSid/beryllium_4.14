911e82920ff5fefb907439ba1b59292793e2a5c6
Pavel Dubrova <pashadubrova@gmail.com>
2021-11-05 13:57:32 +0200

    clk: qcom: Add the clock controller drivers for SDM845
    Add drivers for global, camera, debug, display, gpu and video
    clock controller support on SDM845.
    This snapshot of the camcc-sdm845, debugcc-sdm845, dispcc-sdm845,
    gcc-sdm845, gpucc-sdm845 and videocc-sdm845 drivers as of
    https://source.codeaurora.org/quic/la/kernel/msm-4.9
    commit 7e5c05f ("Merge "msm: camera: jpeg: Ensure in/out map entries are
    within allowed range"").
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

---
+++ include/dt-bindings/clock/qcom,videocc-sdm845.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_MSM_VIDEO_CC_SDM845_H
+#define _DT_BINDINGS_CLK_MSM_VIDEO_CC_SDM845_H
+
+#define VIDEO_CC_APB_CLK					0
+#define VIDEO_CC_AT_CLK						1
+#define VIDEO_CC_QDSS_TRIG_CLK					2
+#define VIDEO_CC_QDSS_TSCTR_DIV8_CLK				3
+#define VIDEO_CC_VCODEC0_AXI_CLK				4
+#define VIDEO_CC_VCODEC0_CORE_CLK				5
+#define VIDEO_CC_VCODEC1_AXI_CLK				6
+#define VIDEO_CC_VCODEC1_CORE_CLK				7
+#define VIDEO_CC_VENUS_AHB_CLK					8
+#define VIDEO_CC_VENUS_CLK_SRC					9
+#define VIDEO_CC_VENUS_CTL_AXI_CLK				10
+#define VIDEO_CC_VENUS_CTL_CORE_CLK				11
+#define VIDEO_PLL0						12
+
+#endif
